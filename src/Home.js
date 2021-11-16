772cd9d490edb754bf026c634da43de5b765b27f
SafiaOmar <90978408+SafiaOmar@users.noreply.github.com>
2021-11-16 14:09:47 -0500

    updated Home css

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,9 @@ import Product from "./Product";
 
 function Home() {
     return (
+        
         <div className="home">
+            <h1> LOCALLY MADE GLOBALLY KNOWN!!!🥥🫒🍍</h1>
            <div className="home_container">
             <img className="home_image"
             src="https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" 
