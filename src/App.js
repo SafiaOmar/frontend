cc8f8d112079029ac550d6e9f8e55ee3b01c332c
SafiaOmar <90978408+SafiaOmar@users.noreply.github.com>
2021-11-16 13:18:46 -0500

    updated product css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import React from "react";
 import './App.css';
 import Login from "./components/Login";
-import "./Home"
 import Home from './Home';
-import "./Product";
+
 
 function App() {
   return (
+   
     <div className="App">
-      <Login />
-      <Home />
-    </div>
+        <Login />
+      <Home />  
+      </div>
+   
   );
 }
 
