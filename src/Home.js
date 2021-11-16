00f717fdb8dff692e543b8bd4b8f899537643843
SafiaOmar <90978408+SafiaOmar@users.noreply.github.com>
2021-11-16 13:41:11 -0500

    updated product images

--- src/Home.js
+++ src/Home.js
@@ -23,7 +23,7 @@ function Home() {
               id="44444656"
               title='Morrocon cooking pot' 
               price={20.99} 
-              image="https://m.media-amazon.com/images/I/91sMB6Ob+CL._AC_UY445_.jpg"  />
+              image="https://ii.worldmarket.com/fcgi-bin/iipsrv.fcgi?FIF=/images/worldmarket/source/61299_XXX_v1.tif&wid=480&cvt=jpeg"  />
               </div>
             
               <div  className="home_row">
@@ -31,17 +31,17 @@ function Home() {
                 id="26554556"
                 title='African Tea pot' 
                 price={12.99} 
-                image="https://m.media-amazon.com/images/I/91sMB6Ob+CL._AC_UY445_.jpg" />
+                image="https://secure.img1-ag.wfcdn.com/im/00961875/resize-h445%5Ecompr-r85/5438/54389589/Brookhill+Steel+Dotted+Asian+0.84+-qt.+Cast+Iron+Teapot.jpg" />
             <Product 
             id="100000236"
             title='Mabuyu (oragnic Baobab)' 
             price={10.99} 
-            image="https://m.media-amazon.com/images/I/91sMB6Ob+CL._AC_UY445_.jpg" />
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRASjmj3pdx_uYwE4riGDX9dLkf7bcpsAQtUL_ipEwXW5Cs_Ep0hhFW-1Ttwk-m4EJX7uc&usqp=CAU" />
             <Product
             id="084561231"
             title='African Black Soap' 
             price={5.99} 
-            image="https://m.media-amazon.com/images/I/91sMB6Ob+CL._AC_UY445_.jpg"  />
+            image="https://cdn.shopify.com/s/files/1/0041/0471/6386/products/S077OriginalUnscented_300x300.jpg?v=1614200831"  />
               </div> 
             
               <div  className="home_row">
@@ -49,7 +49,7 @@ function Home() {
               id="4545464646"
               title='African Women Shoes' 
               price={100.99} 
-              image="https://m.media-amazon.com/images/I/91sMB6Ob+CL._AC_UY445_.jpg"/>
+              image="https://sokojanja.com/3482-deal_home_default/woven-sling-bag.jpg"/>
               </div>
         </div>
         </div>
