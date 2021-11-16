e48fa14b436703ac7bd9deee09b28aff27543809
SafiaOmar <90978408+SafiaOmar@users.noreply.github.com>
2021-11-16 12:27:38 -0500

    first push with changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Login from "./components/Login";
+import "./Home"
+import Home from './Home';
+import "./Product";
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Home.css";
+import "./Product";
+import Product from "./Product";
+
+function Home() {
+    return (
+        <div className="home">
+           <div className="home_container">
+            <img className="home_image"
+            src="https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" 
+            alt=""
+        />
+        
+        <div  className="home_row">
+            <Product 
+            id="123456"
+            title='Dashiki Shirt' 
+            price={19.99} 
+            image="https://m.media-amazon.com/images/I/91sMB6Ob+CL._AC_UY445_.jpg" />
+
+            <Product
+              id="44444656"
+              title='Morrocon cooking pot' 
+              price={20.99} 
+              image="https://m.media-amazon.com/images/I/91sMB6Ob+CL._AC_UY445_.jpg"  />
+              </div>
+            
+              <div  className="home_row">
+              <Product 
+                id="26554556"
+                title='African Tea pot' 
+                price={12.99} 
+                image="https://m.media-amazon.com/images/I/91sMB6Ob+CL._AC_UY445_.jpg" />
+            <Product 
+            id="100000236"
+            title='Mabuyu (oragnic Baobab)' 
+            price={10.99} 
+            image="https://m.media-amazon.com/images/I/91sMB6Ob+CL._AC_UY445_.jpg" />
+            <Product
+            id="084561231"
+            title='African Black Soap' 
+            price={5.99} 
+            image="https://m.media-amazon.com/images/I/91sMB6Ob+CL._AC_UY445_.jpg"  />
+              </div> 
+            
+              <div  className="home_row">
+              <Product 
+              id="4545464646"
+              title='African Women Shoes' 
+              price={100.99} 
+              image="https://m.media-amazon.com/images/I/91sMB6Ob+CL._AC_UY445_.jpg"/>
+              </div>
+        </div>
+        </div>
+    )
+}
+
+export default Home
