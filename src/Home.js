9fa815ec8ae835c56022f903d27e9e8e87205839
SafiaOmar <90978408+SafiaOmar@users.noreply.github.com>
2021-11-16 15:40:40 -0500

    taking a break

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import "./Home.css";
 import "./Product";
 import Product from "./Product";
@@ -16,13 +17,13 @@ function Home() {
         
         <div  className="home_row">
             <Product 
-            id="123456"
+            id="1"
             title='Dashiki Shirt' 
             price={19.99} 
             image="https://m.media-amazon.com/images/I/91sMB6Ob+CL._AC_UY445_.jpg" />
 
             <Product
-              id="44444656"
+              id="2"
               title='Morrocon cooking pot' 
               price={20.99} 
               image="https://ii.worldmarket.com/fcgi-bin/iipsrv.fcgi?FIF=/images/worldmarket/source/61299_XXX_v1.tif&wid=480&cvt=jpeg"  />
@@ -30,17 +31,17 @@ function Home() {
             
               <div  className="home_row">
               <Product 
-                id="26554556"
+                id="3"
                 title='African Tea pot' 
                 price={12.99} 
                 image="https://secure.img1-ag.wfcdn.com/im/00961875/resize-h445%5Ecompr-r85/5438/54389589/Brookhill+Steel+Dotted+Asian+0.84+-qt.+Cast+Iron+Teapot.jpg" />
             <Product 
-            id="100000236"
+            id="4"
             title='Mabuyu (oragnic Baobab)' 
             price={10.99} 
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRASjmj3pdx_uYwE4riGDX9dLkf7bcpsAQtUL_ipEwXW5Cs_Ep0hhFW-1Ttwk-m4EJX7uc&usqp=CAU" />
             <Product
-            id="084561231"
+            id="5"
             title='African Black Soap' 
             price={5.99} 
             image="https://cdn.shopify.com/s/files/1/0041/0471/6386/products/S077OriginalUnscented_300x300.jpg?v=1614200831"  />
@@ -48,7 +49,7 @@ function Home() {
             
               <div  className="home_row">
               <Product 
-              id="4545464646"
+              id="6"
               title='African Women Shoes' 
               price={100.99} 
               image="https://sokojanja.com/3482-deal_home_default/woven-sling-bag.jpg"/>
